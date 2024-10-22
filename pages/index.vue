f77d91ad750da806473e80c898b38eecee153230
Viacheslav <slavavorobey@vk.com>
2024-10-22 22:25:50 +0300

    add style button

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<button id="install-button" style="display: none">
+		<button id="install-button" style="display: none" class="install">
 			Установить приложение
 		</button>
 		<CommonHero :film="randomFilm" />
@@ -29,3 +29,19 @@ onMounted(async () => {
 })
 const randomFilm = computed(() => useFilmsStore().oneFilm)
 </script>
+
+<style>
+.install {
+	position: fixed;
+	bottom: 20px;
+	right: 20px;
+	left: 20px;
+	padding: 10px 20px;
+	border-radius: 10px;
+	background: rgb(0, 95, 95);
+	color: rgb(255, 255, 255);
+	font-weight: 700;
+	font-size: 16px;
+	z-index: 1000;
+}
+</style>
